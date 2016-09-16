777a92b7cfebf7ec9baaa49c45818c455d4cd836
ArthurSampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2016-09-16 17:13:33 -0300

    I started the next analysis

---
+++ Inference/MoviesAnalytics/server.R
@@ -0,0 +1,71 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+library(shiny)
+library(dplyr, warn.conflicts = F)
+library(ggplot2)
+library(rsconnect)
+library(cluster)
+library(plotly)
+require(GGally)
+library(ggdendro)
+library(knitr)
+library(resample)
+theme_set(theme_bw())
+
+
+#Carregando datasets
+
+ratings <- read.csv("~/Documentos/SegundoPeriodo/DataAnalysis/Inference/ml-latest-small/ratings.csv")
+movies <- read.csv("~/Documentos/SegundoPeriodo/DataAnalysis/Inference/ml-latest-small/movie-genre.csv")
+ratings.filme <- read.csv("~/Documentos/SegundoPeriodo/DataAnalysis/Inference/ml-latest-small/ratings-por-filme.csv")
+
+#preparando o dataset para responder a primeira questão
+
+GENEROS = c("Action", "Comedy", "Musical", "Documentary", "Drama", "Romance", "Horror", "Thriller")
+movies$genre = as.character(movies$genre)
+#encontra os filmes que serão estudados
+movies.analyzed = movies %>% filter(movies$genre %in% GENEROS)
+#filtrando os rantings 
+ratings.analyzed = ratings %>% filter(ratings$movieId %in% movies.analyzed$movieId)
+
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+   
+  output$distPlot <- renderPlot({
+    
+    # generate bins based on input$bins from ui.R
+    x    <- faithful[, 2] 
+    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    
+    # draw the histogram with the specified number of bins
+    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+  })
+  
+})
+
+library(streamgraph)
+
+# current verison
+packageVersion("streamgraph")
+
+library(dplyr)
+
+ggplot2movies::movies %>%
+  select(year, Action, Animation, Comedy, Drama, Documentary, Romance, Short) %>%
+  tidyr::gather(genre, value, -year) %>%
+  group_by(year, genre) %>%
+  tally(wt=value) -> dat
+
+streamgraph(dat, "genre", "n", "year", interactive=TRUE) %>%
+  sg_axis_x(20, "year", "%Y") %>%
+  sg_fill_brewer("PuOr")
+
---
+++ Inference/MoviesAnalytics/ui.R
@@ -0,0 +1,59 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Uma análise dos filmes"),
+  h4("Como se comportam os admirados da sétima arte?", align = "center"),
+  h5("Por", tags$a(href= "https://www.linkedin.com/in/arthursampaiopcorreia?", "Arthur Sampaio"), align = "right"),
+  
+
+  h2("Resumo"),
+  p("Neste pequeno relatório utilizei dados coletados por um sistema de recomendação, o ", tags$a(href="http://movielens.org", "MovieLens")," 
+    sobre as avaliações de usuários sobre um conjunto de filmes. Tenho o objetivo de responder duas perguntas sugeridas utilizando 
+    os conhecimentos adquiridos na jornada de Análise de Dados. A análise foi feita na linguagem R com o auxilio de diversas bibliotecas 
+    com o auxilio do RMarkdown para gerar este documento", align = "justify"),
+  
+  
+  h2("Os dados"),
+  
+  p("Serão estudados os dados coletados pelo MovieLens sobre avaliações de um catálogo de mais de 10 mil filmes.
+    Os dados foram criados por 668 usuários durante 03 de Abril de 1996 e 09 de Janeiro de 2016. Os dados, distribuidos em quatro datasets, 
+    constituem cerca de 10 variáveis com informações de desde o título até as notas obtidas por cada um dos usuários."),
+  p("Para nortear a nossa análise irei responder algumas perguntas nos próximos tópicos."),
+  
+  h2("Como é o mapa anual dos gêneros?"),
+  
+  p("O cinema é uma das artes que mais possuem gêneros. Assim como a sociedade, o cinema se reinventa paralelo as transformações sociais, políticas e
+    econômicas, por conta disto novos gêneros de filmes são inventados todos os anos. Por conta disto, pesquisei quais são os principais gêneros de filmes,
+    e segundo o", tags$a(href ="http://cultura.culturamix.com/eventos/cinema/generos-do-cinema-conheca-os-principais", "CulturaMix"), "os principais gêneros
+    são:", tags$em("Ação, Musical, Documentário, Comédia, Drama, Romance e Supense/Terror.")),
+  p("O objetivo desta seção é compreender em quais épocas do ano determinado gênero é mais popular entre as pessoas que assistem filmes regularmente."),
+  
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+       sliderInput("bins",
+                   "Number of bins:",
+                   min = 1,
+                   max = 50,
+                   value = 30)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+       plotOutput("distPlot")
+    )
+  )
+))
